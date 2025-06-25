947906d5722af192c49c110e47b5c7347a78b19b
Albert Wu <albertwu0001@gmail.com>
2025-06-25 10:45:21 -0700

    route settings

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -7,7 +7,7 @@ import { useApi } from '../../lib/useApi';
 import { useTranslation } from 'react-i18next';
 
 export default function ForgotPasswordPage() {
-  const { t } = useTranslation('common');
+  const { t, i18n } = useTranslation('common');
   const { request } = useApi();
   const [email, setEmail] = useState('');
   const [sent, setSent] = useState(false);
