0d2bf8718531506b68bc6af0e7b7c98a91e0db7a
Albert Wu <albertwu0001@gmail.com>
2025-06-26 09:56:42 -0700

    feat(haptic button): button ui changed

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { Slot } from '@radix-ui/react-slot'
+import { motion } from 'framer-motion'
 
 import { cn } from '../../lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background cursor-pointer select-none',
   {
     variants: {
       variant: {
@@ -34,11 +35,50 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  hapticEffect?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, disabled = false, hapticEffect = true, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
+
+    const handleClick = (event: React.MouseEvent) => {
+      const { onClick } = props 
+      if (hapticEffect) {
+        if ('vibrate' in navigator) {
+          navigator.vibrate(100); 
+        }
+      }
+
+      if (onClick) {
+        onClick(event as React.MouseEvent<HTMLButtonElement>)
+      }
+
+      if (event.defaultPrevented) return;
+    };
+    
+    if (hapticEffect) {
+      return (
+        <motion.div
+          whileHover={!disabled ? {
+            translateY: '-1px',
+          } : {}}
+          whileTap={!disabled ? {
+            scale: 0.95, 
+          } : {}}
+          className={cn(buttonVariants({ variant, size, className }))}
+          onClick={handleClick}
+        >
+          <Comp
+            ref={ref}
+            {...props}
+            className="w-full h-full"
+            disabled={disabled}
+          />
+        </motion.div>
+      )
+    }
+    
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
