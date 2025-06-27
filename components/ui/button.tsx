bf1bb597fbc722bb3b77c1a2189ca3dd94f820e8
Albert Wu <albertwu0001@gmail.com>
2025-06-26 20:43:09 -0700

    haptic feedback

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,6 +4,7 @@ import { Slot } from '@radix-ui/react-slot'
 import { motion } from 'motion/react'
 import { cn } from '../../lib/utils'
 import LoadingDots from './loading-dots'
+import { useHaptic } from "use-haptic"
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background cursor-pointer select-none',
@@ -40,24 +41,25 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, disabled = false, hapticEffect = true, ...props }, ref) => {
-    const [loading, setLoading] = React.useState(false);  
-    const Comp = asChild ? Slot : 'button'
+    const [loading, setLoading] = React.useState(false);
+    const { triggerHaptic } = useHaptic();
+    const Comp = asChild ? Slot : "button";
 
     const handleClick = async (event: React.MouseEvent) => {
       const { onClick } = props;
 
       event.stopPropagation();
-      
+
       if (hapticEffect) {
-        if ('vibrate' in navigator) {
-          navigator.vibrate(100); 
-        }
+        triggerHaptic();
       }
 
       if (onClick) {
-        setLoading(true)
-        await Promise.resolve(onClick(event as React.MouseEvent<HTMLButtonElement>))
-        setLoading(false)
+        setLoading(true);
+        await Promise.resolve(
+          onClick(event as React.MouseEvent<HTMLButtonElement>)
+        );
+        setLoading(false);
       }
 
       if (event.defaultPrevented) return;
